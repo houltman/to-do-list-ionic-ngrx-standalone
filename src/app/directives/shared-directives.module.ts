5074c8d87879204f5bf682f0d508b14359dd638b
houltman <houltman_gonzalez@hotmail.com>
2024-07-13 15:51:56 -0400

    delete directive for netlify

--- src/app/directives/shared-directives.module.ts
+++ src/app/directives/shared-directives.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AutofocusDirective } from 'src/app/directives/autofocus.directive';
+//import { AutofocusDirective } from 'src/app/directives/autofocus.directive';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective],
-  exports: [AutofocusDirective],
+  declarations: [],
+  exports: [],
   imports: [
     CommonModule
   ]
