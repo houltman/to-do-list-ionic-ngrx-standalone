e94f4d55b060e03bdf4dcfcd5be21994bcb63525
houltman <houltman_gonzalez@hotmail.com>
2024-07-13 12:46:07 -0400

    add capacitor,Readme, document

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -3,22 +3,22 @@ import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'redux',
         loadComponent: () =>
           import('../tab1/tab1.page').then((m) => m.Tab1Page),
       },
       {
-        path: 'tab2',
+        path: 'localstorage',
         loadComponent: () =>
           import('../tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/redux',
         pathMatch: 'full',
       },
     ],
